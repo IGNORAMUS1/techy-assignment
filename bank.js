7df15324cf5a5655723d5f54b67f847ba085f40d
IGNORAMUS1 <folorunshoopeyemi123456@gmail.com>
2024-04-20 05:05:46 +0100

    New features: withdraw Limit and Transfer feature

--- bank.js
+++ bank.js
@@ -4,7 +4,9 @@ class Useraccount {
         this._firstName = firstName;
         this._lastName = lastName;
         this._balance = 0;
-        this._transaction = [];   
+        this._transaction = [];
+        this._withdrawLimit = 3;
+        this._counter = 0;   
     }
     get accountHolder() {
         return `${this._firstName} ${this._lastName}`;
@@ -20,18 +22,30 @@ class Useraccount {
     };
 
     set withdraw(amount) {
-            if (this._balance < amount) {
-                this._balance -= 0;
-            } else {
-                this._balance -= amount;
-                this._transaction.push(new Transaction('withdraw', amount));
-            }
+        if (this._balance < amount || this._counter == this._withdrawLimit) {
+            this._balance -= 0; 
+        }else {
+            this._balance -= amount;
+            this._transaction.push(new Transaction('withdraw', amount));
+            this._counter ++;
+        };
     };
     
     get transactions() {
         return this._transaction;
     };
-    
+
+    transfer(amount, user) {
+        if (amount > this._balance) {
+            return `Not sufficient fund`;
+        } if (typeof user !== `object`) {
+            return `User does not exist!`;
+        } else {
+            return this._balance -= amount,
+            user.deposit = amount;
+        }
+    }
+ 
 }
 
 class Transaction {
@@ -43,15 +57,15 @@ class Transaction {
 
     get type() {
         return this._type;
-    }
+    };
 
     get amount() {
         return this._amount;
-    }
+    };
 
     get date() {
         return this._date.toString;
-    }
+    };
 }
 
 
@@ -61,7 +75,15 @@ const user1 = new Useraccount(3588495039, 'John', 'Doe');
 // console.log(user1.accountHolder);
 // console.log(user1.balance);
 
-// user1.deposit = 100;
+user1.deposit = 1000;
+
+// user1.withdraw = 150;
+// console.log(user1.balance);
+
+// user1.withdraw = 150;
+// console.log(user1.balance);
+
+// user1.withdraw = 150;
 // console.log(user1.balance);
 
 // user1.withdraw = 150;
@@ -70,4 +92,9 @@ const user1 = new Useraccount(3588495039, 'John', 'Doe');
 // console.log(user1.transactions);
 
 const user2 = new Useraccount(3364879867, "Johnson", "Goerge")
+
+// Transfer 500 from user1 to user2.
+
+user1.transfer(500, user2)
+console.log(user2._balance)
 console.log(user2.transactions)
