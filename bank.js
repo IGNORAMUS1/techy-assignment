6e02946952ea53cb60a5f8e8c0edeb6e6cbcdac5
IGNORAMUS1 <folorunshoopeyemi123456@gmail.com>
2024-04-28 04:35:00 +0100

    withdraw limit and transaction tracker updated

--- bank.js
+++ bank.js
@@ -5,8 +5,8 @@ class Useraccount {
         this._lastName = lastName;
         this._balance = 0;
         this._transaction = [];
-        this._withdrawLimit = 3;
-        this._counter = 0;   //withdrawal limit counter
+        this._withdrawLimit = 3000;
+        this._totalWithdraw = 0;
     }
     get accountHolder() {
         return `${this._firstName} ${this._lastName}`;
@@ -16,37 +16,62 @@ class Useraccount {
         return this._balance;
     };
 
+    // allow user to change their daily limit
+    set changeLimit (amount) {
+        this._withdrawLimit = amount;
+    }
+
+    // Deposite function
+
     deposit(amount) {
         this._balance += amount;
         this._transaction.push(new Transaction('deposit', amount));
     };
 
+    // transfer receiver
+
+    set receive(amount) {
+        this._balance += amount;
+        this._transaction.push(new Transaction('receive', amount));
+    }
+
+    // Withdraw function
+
     withdraw(amount) {
         if (this._balance < amount) {
             this._balance -= 0;
-            return `Not sufficient fund`; 
-        }
-        if (this._counter == this._withdrawLimit){
-            return `You've reached your daily limit.`
-        }else {
+            console.log(`Not sufficient fund`); 
+        } else if (amount > this._withdrawLimit) {
+            console.log(`This amount have exceeded your limit!`)
+        } else if (this._totalWithdraw == this._withdrawLimit){
+            console.log(`You've reached your daily limit.`)
+        } else {
             this._balance -= amount;
             this._transaction.push(new Transaction('withdraw', amount));
-            this._counter ++;
+            return this._totalWithdraw += amount;
         };
     };
     
+    // get transactions function
     get transactions() {
         return this._transaction;
     };
 
+    // transfer function
     transfer(amount, user) {
         if (amount > this._balance) {
-            return `Not sufficient fund`;
-        } if (typeof user !== `object`) {
-            return `User does not exist!`;
+            console.log(`Not sufficient fund`);
+        } else if (typeof user !== `object`) {
+            console.log(`User does not exist!`);
+        } else if (amount > this._withdrawLimit) {
+            console.log(`This amount have exceeded your limit!`);
+        } else if(this._totalWithdraw == this._withdrawLimit) {
+            console.log(`You've reached your daily limit.`)
         } else {
             return this._balance -= amount,
-            user.deposit(amount);
+            this._transaction.push(new Transaction('Transfer', amount)),
+            user.receive = amount,
+            this._totalWithdraw += amount;
         }
     }
  
@@ -79,17 +104,21 @@ const user1 = new Useraccount(3588495039, 'John', 'Doe');
 // console.log(user1.accountHolder);
 // console.log(user1.balance);
 
-user1.deposit(1000);
+user1.deposit(4000);
 
-user1.withdraw(150);
-console.log(user1.balance);
+// user1.withdraw(150);
+// console.log(user1.balance);
 
-console.log(user1.transactions);
+// console.log(user1.transactions);
 
 const user2 = new Useraccount(3364879867, "Johnson", "Goerge")
 
-// Transfer 500 from user1 to user2.
+// Transfer from user1 to user2.
 
-user1.transfer(500, user2)
-console.log(user2._balance)
-console.log(user2.transactions)
+user1.transfer(1500, user2);
+user1.transfer(1500, user2);
+//user1.transfer(4000, user2) // this line will console 'This amount have exceeded your limit'.
+console.log(user2._balance);
+user1.transfer(500, user2); // this line will console 'You've reached your daily limit.
+console.log(user2.transactions);
+console.log(user1.transactions);
